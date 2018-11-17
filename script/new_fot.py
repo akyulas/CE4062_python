dca859ce58151d6752fceed2741ef1b87336311f
akyulas <bolshack_fire@hotmail.com>
2018-11-18 03:07:57 +0800

    check if test nm new fot can be parsed 10

--- script/new_fot.py
+++ script/new_fot.py
@@ -11,6 +11,7 @@ class NewFot(object):
 
     def start(self):
         crash_files = glob.glob(self.new_fot_crash_directory)
+        print(crash_files)
         crash_files_iter = iter(crash_files)
         gdb_wrapper = GDB_Wrapper(self.new_fot_file_name,
                                   self.new_fot_run_command, crash_files_iter,
