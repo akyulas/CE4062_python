cba65709ca1f3ee03a52f1eb0677397a06d2e4bb
akyulas <bolshack_fire@hotmail.com>
2018-11-17 16:42:27 +0800

    testing input from command line 2

--- script/__init__.py
+++ script/__init__.py
@@ -1,3 +1,10 @@
 if __name__ == '__main__':
-    file_name = input("What's the file that you want to triage?")
+    file_name = input("What's the file number that you want to triage?")
+    print("1. test-cxxfilt-fot")
+    print("2. test-objdump-fot")
+    print("3. test-xed-fot")
+    print("4. test-mjs-fot")
+    print("5. test-readelf-fot")
+    print("6. test-nm-new-fot")
+    print("7. test-strings-fot")
     print(file_name)
